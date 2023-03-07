d4f0065ec9edb2828038f36c4c84f2c108b9ef64
youssef917 <o@gmail.com>
2023-03-07 17:35:30 +0200

    first commit

---
+++ src/Context/Cart/CartTypes.js
@@ -0,0 +1,8 @@
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+export const INCREASE = "INCREASE";
+export const DECREASE = "DECREASE";
+export const CHECKOUT = "CHECKOUT";
+export const CLEAR = "CLEAR";
+export const ADD_FAVO = "ADD_FAVO";
+export const REM_FAVO = "REM_FAVO";
---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const products = [
+  // {
+  //   id: 1,
+  //   name: "Cerveza Modelo",
+  //   price: 919.11,
+  //   image: require("./assets/img/1.png"),
+  // },
+  // {
+  //   id: 2,
+  //   name: "Diesel Life",
+  //   price: 1257.92,
+  //   image: require("./assets/img/2.png"),
+  // },
+  {
+    id: 3,
+    name: "Indian Cricket Team jersey",
+    price: 1500.85,
+    image: require("./assets/img/3.png"),
+  },
+  {
+    id: 4,
+    name: "One Punch man - OK",
+    price: 1250.9,
+    image: require("./assets/img/4.png"),
+  },
+  {
+    id: 5,
+    name: "Hiking jacket",
+    price: 1750.85,
+    image: require("./assets/img/5.png"),
+  },
+  {
+    id: 6,
+    name: "Real Heart",
+    price: 3100.61,
+    image: require("./assets/img/6.png"),
+  },
+  {
+    id: 7,
+    name: "Fredd - Black and White",
+    price: 1801.1,
+    image: require("./assets/img/7.png"),
+  },
+  {
+    id: 8,
+    name: "Star Wars - The Last",
+    price: 1199.99,
+    image: require("./assets/img/8.png"),
+  },
+  {
+    id: 9,
+    name: "Yellow Blouse",
+    price: 2395.16,
+    image: require("./assets/img/9.png"),
+  },
+  {
+    id: 10,
+    name: "Rick and Morty - Supreme",
+    price: 1243.82,
+    image: require("./assets/img/10.png"),
+  },
+];
